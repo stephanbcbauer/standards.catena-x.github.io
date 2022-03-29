2bce7f269b281d68f4bdf4f23fa1b7716774b059
Siegfried Kiermayer <siegfried.kiermayer@sap.com>
2022-03-29 19:20:37 +0200

    update projectName after moving to new repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'catenax-ng', // Usually your GitHub org/user name.
-  projectName: 'acme', // Usually your repo name.
+  projectName: 'catenax-ng.github.io', // Usually your repo name.
 
   presets: [
     [
